8809234201b39d2bb1cc78edbef9b5840610661c
dnwakalor-a11y <dnwakalor@gmail.com>
2025-12-05 22:23:50 -0500

    Delete modules directory

--- modules/platform-admin/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './platform-admin.controller';
-export * from './platform-admin.service';
-export * from './platform-admin.module';
-export * from './dto/platform-admin.dto';
--- modules/system-config/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './system-config.entity';
-export * from './system-config.service';
-export * from './system-config.module';
